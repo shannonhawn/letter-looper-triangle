96d414b8874c3fdee05270bb2379fe2bacbdfd98
Shannon Hawn <shannonhawn@hotmail.com>
2016-02-26 17:02:37 -0500

    put in boiler plate

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var aletter = prompt('Enter a letter');
+var counter;
+var counter1;
+
+for (counter = 0; counter < 10; counter++) {
+  // For-loop
+  document.write(aletter);
+}
+
+document.write('<br>');
+
+for (counter = 0; counter1 <= counter; counter++) {
+  // For-loop
+}
